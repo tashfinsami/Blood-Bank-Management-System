1a0572e5508871e50f709d17bf71c0f1dfccfac1
tashfinsami <tashfinsami@gmail.com>
2022-06-13 18:51:37 +0600

    Add Main.java

---
+++ src/bloodbank/Main.java
@@ -0,0 +1,12 @@
+package bloodbank;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("GKJKHL\n" + "jjkhjh\n");
+        //<default package>.login().setVisible(true);
+    }
+    
+}

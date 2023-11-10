344678b9d16fc4218a64c9b32c88f87485b187c0
tashfinsami <tashfinsami@gmail.com>
2023-11-10 19:22:39 +0600

    Add stock.sql

---
+++ db/stock.sql
@@ -0,0 +1,10 @@
+create table stock (bloodGroup varchar(20), units int(10));
+
+insert into stock values('A+','0');
+insert into stock values('A-','0');
+insert into stock values('B+','0');
+insert into stock values('B-','0');
+insert into stock values('O+','0');
+insert into stock values('O-','0');
+insert into stock values('AB+','0');
+insert into stock values('AB-','0');
